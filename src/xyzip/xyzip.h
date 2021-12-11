1332ceab967429890411adb6893c1daa5f02850e
byfxxm <byfxxm@outlook.com>
2021-12-11 19:44:00 +0800

    22

--- src/xyzip/xyzip.h
+++ src/xyzip/xyzip.h
@@ -11,11 +11,9 @@
 extern "C"
 {
 #endif
-
 	XYZIP_AIP bool xy_zip(const char* dest, const char* src);
 	XYZIP_AIP bool xy_unzip(const char* dest, const char* src);
 	XYZIP_AIP void xy_setk(uint32_t key);
-
 #ifdef __cplusplus
 }
 #endif
